2c324fe7eafff376d9ad3fc8cea40eb7ba390eab
Gökdeniz Dağ <mountainskysea07@gmail.com>
2024-09-02 15:07:54 +0300

    created: update_info_table() updated: process_emails() process_email() process_words() process_word() update_probability() calculate_probability() get_spam_count() get_ham_count()

--- maths/bayesian-analysis/bsd.py
+++ maths/bayesian-analysis/bsd.py
@@ -47,13 +47,18 @@ def load_data()->pd.DataFrame:
 
 def process_emails(cur:sqlite3.Connection.cursor,data:pd.DataFrame):
     for index,row in data.iterrows():
-        process_email(row)
+        process_email(cur,row)
 
+def update_info_table(cur:sqlite3.Connection.cursor,row:pd.Series):
+    if row[1]:
+        cur.execute('UPDATE info SET data=data+1 WHERE data_label=?',('spammails'))
+    else:
+        cur.execute('UPDATE info SET data=data+1 WHERE data_label=?',('hammails'))
 
-def process_email(row:pd.Series):
+def process_email(cur:sqlite3.Connection.cursor,row:pd.Series):
     words=tokenize_email(row)
     is_spam=bool(row["spam"])
-    process_words(words,is_spam)
+    process_words(cur,words,is_spam)
 
 
 def tokenize_email(row:pd.Series)->List[str]:
@@ -61,9 +66,9 @@ def tokenize_email(row:pd.Series)->List[str]:
     return words
 
 
-def process_words(words:List[str],is_spam:bool):
+def process_words(cur:sqlite3.Connection.cursor,words:List[str],is_spam:bool):
     for word in words:
-        update_word(word,is_spam)
+        update_word(cur,word,is_spam)
 
 
 def update_word(cur:sqlite3.Connection.cursor,word:str,is_spam:bool):
@@ -90,16 +95,20 @@ def update_probability(cur:sqlite3.Connection.cursor):
     cur.execute("SELECT * FROM words")
     words = cur.fetchall()
     for word in words:
-        calculate_probability(cur,word)
-def calculate_probability(cur:sqlite3.Connection.cursor,word:tuple):
-    cur.execute("SELECT * FROM words WHERE word=?",(word[0]))
+        (spam_probability,ham_probability)=calculate_probability(cur,word)
+        cur.execute("UPDATE words SET spam_probability=?,ham_probability=?",(spam_probability,ham_probability))
+def calculate_probability(cur:sqlite3.Connection.cursor,word:tuple)->tuple:
+    spam_probability=(word[2]+2)/get_spam_count()
+    ham_probability=(word[3]+2)/get_ham_count()
+    return (spam_probability,ham_probability)
+    
 def get_spam_count(cur:sqlite3.Connection.cursor)->int:
-    cur.execute("SELECT * FROM info WHERE data_label=?",('spam'))
+    cur.execute("SELECT * FROM info WHERE data_label=?",('spammails'))
     spam_data=cur.fetchone()
     return spam_data[1]
 
 def get_ham_count(cur:sqlite3.Connection.cursor)->int:
-    cur.execute("SELECT * FROM info WHERE data_label=?",('ham'))
+    cur.execute("SELECT * FROM info WHERE data_label=?",('hammails'))
     ham_data=cur.fetchone()
     return ham_data[1]
 train()
